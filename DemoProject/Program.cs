27ee0adb7b607cff06b6819ed29000371e0875f2
Kresten Laust <krestenlaust@gmail.com>
2023-08-05 22:54:01 +0200

    Started making demoproject

--- DemoProject/Program.cs
+++ DemoProject/Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleGUI;
+using GUI;
+using GUI.Console;
+using GUI.Console.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DemoProject
 {
     internal class Program
     {
+        static IHost host;
+
         static void Main(string[] args)
         {
+            // Setup DI
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+            builder.Services.AddTransient<IConsole, ConsoleImpl>();
+            builder.Services.AddTransient<IRenderer, ConsoleRenderer>();
+            host = builder.Build();
+
+            // Start application
+            GUIManager manager = new GUIManager(host.Services.GetRequiredService<IRenderer>());
+        }
+
+        static Control InstantiateGUI()
+        {
+            GroupControl main = new GroupControl();
+            
+            main.AddControl(new LabelControl());
+
+
+            return main;
         }
     }
 }
