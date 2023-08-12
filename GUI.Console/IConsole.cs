fb7a9867f0c401bcf8611de68ef53589d28475f4
Kresten Laust <krestenlaust@gmail.com>
2023-08-12 23:08:38 +0200

    Added method to IConsole and added documentation to members

--- GUI.Console/IConsole.cs
+++ GUI.Console/IConsole.cs
@@ -2,14 +2,47 @@
 {
     public interface IConsole
     {
+        /// <summary>
+        /// Gets the width of the buffer area measured in columns.
+        /// </summary>
         int BufferWidth { get; }
+
+        /// <summary>
+        /// Gets the height of the buffer area measured in rows.
+        /// </summary>
         int BufferHeight { get; }
 
+        /// <summary>
+        /// Gets the width of the window measured in columns.
+        /// </summary>
         int ScreenWidth { get; }
+
+        /// <summary>
+        /// Gets the height of the window measured in rows.
+        /// </summary>
         int ScreenHeight { get; }
+
+        /// <summary>
+        /// Gets the uppermost console window position measured in rows.
+        /// </summary>
         int ScreenTop { get; set; }
+
+        /// <summary>
+        /// Gets the leftmost console window position measured in columns.
+        /// </summary>
         int ScreenLeft { get; set; }
 
+        /// <summary>
+        /// Sets the cursor position in the console buffer.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
         void SetCursorPosition(int left, int top);
+
+        /// <summary>
+        /// Writes text at the location the cursor is currently at.
+        /// </summary>
+        /// <param name="text"></param>
+        void Write(string text);
     }
 }
