b48519d7edf0d24fdf1e76bf53d5fb5cdcda154f
Kresten Laust <krestenlaust@gmail.com>
2023-08-12 10:16:11 +0200

    Added RectangeSegment to GUI.Console

---
+++ GUI.Console.Tests/RectangleSegmentTests.cs
@@ -0,0 +1,44 @@
+namespace GUI.Console.Tests
+{
+    [TestClass]
+    public class RectangleSegmentTests
+    {
+        [TestMethod]
+        public void TestSegmentCalculation2x2()
+        {
+            // arrange
+            char[] fullarray = new char[] { 'A', 'B', 'C', 'D' };
+            int fullarrayWidth = 2;
+            int offsetX = 1;
+            int offsetY = 1;
+            int width = 1;
+            int height = 1;
+
+            // act
+            RectangleSegment<char> segment = new(fullarray, fullarrayWidth, offsetX, offsetY, width, height);
+
+            // assert
+            Assert.AreEqual(fullarray[3], segment[0, 0]);
+        }
+
+        [TestMethod]
+        public void TestSegmentModification2x2()
+        {
+            // arrange
+            char[] fullarray = new char[] { 'A', 'B', 'C', 'D' };
+            int fullarrayWidth = 2;
+            int offsetX = 1;
+            int offsetY = 1;
+            int width = 1;
+            int height = 1;
+
+            // act
+            RectangleSegment<char> segment = new(fullarray, fullarrayWidth, offsetX, offsetY, width, height);
+            segment[0, 0] = 'C';
+
+            // assert
+            Assert.AreEqual('C', fullarray[3]);
+            Assert.AreEqual(fullarray[3], segment[0, 0]);
+        }
+    }
+}
--- GUI.Console/RectangleSegment.cs
+++ GUI.Console/RectangleSegment.cs
@@ -1,7 +1,27 @@
-﻿namespace GUI.Console
+﻿using System;
+
+namespace GUI.Console
 {
-    public class RectangleSegment
+    public class RectangleSegment<T>
     {
-        
+        readonly Memory<T>[] rows;
+
+        public RectangleSegment(T[] array, int width, int offsetX, int offsetY, int segmentWidth, int segmentHeight)
+        {
+            rows = new Memory<T>[segmentHeight];
+
+            for (int i = 0; i < segmentHeight; i++)
+            {
+                rows[i] = new Memory<T>(array, (i + offsetY) * width + offsetX, segmentWidth);
+            }
+        }
+
+        public T this[int x, int y]
+        {
+            get => rows[y].Span[x];
+            set => rows[y].Span[y] = value;
+        }
+
+        public Span<T> GetRow(int y) => rows[y].Span;
     }
 }
